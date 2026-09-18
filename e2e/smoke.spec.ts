acf3e838844ae0ca8904ecce08355d975239bb90
Carl-Erik Kopseng <carlerik@gmail.com>
2026-09-18 19:57:16 +0200

    clean up

--- e2e/smoke.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('renders the Flisplan heading', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page.getByRole('heading', { name: /flisplan/i })).toBeVisible()
-})
--- src/test/setup.ts
+++
@@ -1,3 +0,0 @@
-import '@testing-library/jest-dom'
-import 'fake-indexeddb/auto'
-if (!globalThis.ResizeObserver) globalThis.ResizeObserver = class { observe() {} unobserve() {} disconnect() {} } as typeof ResizeObserver
